0082422b4e8b9dc5663fa60e30d1545104bff86e
PiotrRozczka <90185079+PiotrRozczka@users.noreply.github.com>
2021-10-06 15:44:03 +0200

    Add files via upload

---
+++ programowanieObiektowe/lekcja7/6_wskazniki.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Worker{
+   public:
+      string name, surname;
+      void getData();
+};
+
+void Worker::getData(){
+   cout << "Imiê i nazwisko: " << name << " " << surname << endl;
+}
+
+int main(){
+   setlocale(LC_CTYPE,"polish");
+   
+   Worker kowalski;
+   Worker *p_kowalski;
+   
+   p_kowalski = &kowalski;
+   
+   cout << "Adres obiektu kowalski: " <<p_kowalski << endl;
+   
+   p_kowalski->name="Anna";
+   kowalski.surname = "Kowalski";
+   
+   kowalski.getData();
+   p_kowalski->getData();
+   
+   return 0;
+}
