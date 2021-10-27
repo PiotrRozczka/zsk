ae9be5dc760cabd4240c8afcf7fae7d6186c4100
PiotrRozczka <90185079+PiotrRozczka@users.noreply.github.com>
2021-10-27 15:09:57 +0200

    Update 9.1.cpp

--- programowanieObiektowe/lekcja9/9.1.cpp
+++ programowanieObiektowe/lekcja9/9.1.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using namespace std;
 
 class Worker{
@@ -8,36 +7,26 @@ class Worker{
 		string name {"Katarzyna"};
 		string surname {"Nowak"};
 		
-		Worker();
-		Worker(int pId, string pName, string pSurname);
+		Worker(int pId = 1, string pName = "Name", string pSurname = "Surname");
 		
 		void getData();
 		
+		
 };
 
-Worker::Worker(){
-	id=13;
-	name="DOMYŚLNE IMIĘ";
-	surname="DOMYŚLNE NAZWISKO";
-}
-
-Worker::Worker(int pId, string pName, string pSurname){
-	id = pId;
-	surname = pSurname;
-	name = pName;
-}
-
+	Worker::Worker(int pId, string pName, string pSurname){
+		
+		id=pId;
+		name=pName;
+		surname=pSurname;
+		
+	};
 void Worker::getData(){
-	cout<<"Id: " << id << "\nImię: " << name << ", nazwisko: " << surname << endl;
-}
+		cout << "Id: " << id << "\nImiê: " << name << "\nNazwisko: " << surname << endl;
+	}
 
 int main(){
 	setlocale(LC_CTYPE, "polish");
-	Worker nowak;
-	nowak.getData();
-	
-	Worker nowak2 = Worker();
-	nowak2.getData();
 	
 	Worker nowak1 = Worker(10, "Krystyna", "Pawlak");
 	nowak1.getData();
