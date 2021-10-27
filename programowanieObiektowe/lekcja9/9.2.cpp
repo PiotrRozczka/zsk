ba9bf1ff7d0a7d980dbc0706a95b63cf710e9b75
PiotrRozczka <90185079+PiotrRozczka@users.noreply.github.com>
2021-10-27 15:14:48 +0200

    Add files via upload

---
+++ programowanieObiektowe/lekcja9/9.2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+class Worker{
+	public:
+		int id {8};
+		string name {"Katarzyna"};
+		string surname {"Nowak"};
+		
+		Worker();
+		Worker(int pId, string pName, string pSurname);
+		
+		void getData();
+		
+};
+
+Worker::Worker(){
+	id=13;
+	name="DOMYŒLNE IMIÊ";
+	surname="DOMYŒLNE NAZWISKO";
+}
+
+Worker::Worker(int pId, string pName, string pSurname){
+	id = pId;
+	surname = pSurname;
+	name = pName;
+}
+
+void Worker::getData(){
+	cout<<"Id: " << id << "\nImiê: " << name << ", nazwisko: " << surname << endl;
+}
+
+int main(){
+	setlocale(LC_CTYPE, "polish");
+	Worker nowak;
+	nowak.getData();
+	
+	Worker nowak2 = Worker();
+	nowak2.getData();
+	
+	Worker nowak1 = Worker(10, "Krystyna", "Pawlak");
+	nowak1.getData();
+	
+	return 0;
+}
